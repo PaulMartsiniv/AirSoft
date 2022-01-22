9cfe65aecf299273aa53c97727f25d7ac969c74a
Pavlo_Martsiniv <imitator2010@gmail.com>
2022-01-22 13:39:01 +0200

    Initial commit

---
+++ src/main/java/synergy/AirSoftApplication.java
@@ -0,0 +1,13 @@
+package synergy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirSoftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirSoftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/synergy/AirSoftApplicationTests.java
@@ -0,0 +1,13 @@
+package synergy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirSoftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
